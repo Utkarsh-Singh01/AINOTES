9859fb0888078ff5c0d8c673bea6ea585198675c
Utkarsh Singh <us6608818@gmail.com>
2026-06-04 23:42:51 +0530

    Update index.js

--- Back-Project/AINotes/Backend/index.js
+++ Back-Project/AINotes/Backend/index.js
@@ -20,7 +20,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin: "http://localhost:5173",
+    {origin: "https://ainotes-1-utqt.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
