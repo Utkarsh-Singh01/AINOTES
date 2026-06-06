5368df7a123d9a38479cfc001696dfae6ded1640
Utkarsh Singh <us6608818@gmail.com>
2026-06-06 10:21:37 +0530

    Update api.js

--- Back-Project/AINotes/Frontend/src/services/api.js
+++ Back-Project/AINotes/Frontend/src/services/api.js
@@ -4,7 +4,8 @@ import { setUserData } from "../redux/userSlice";
 import { store } from "../redux/store";
 
 const getAuthHeader = () => {
-    const token = store.getState().user.token
+    
+    const token = store.getState().user.token || localStorage.getItem("token")
     return token ? { Authorization: `Bearer ${token}` } : {}
 }
 
@@ -12,7 +13,7 @@ export const getCurrentUser = async (dispatch) => {
     try {
         const result = await axios.get(serverUrl + "/api/user/currentuser", {
             withCredentials: true,
-            headers: getAuthHeader()  
+            headers: getAuthHeader()
         });
         dispatch(setUserData(result.data));
     } catch (error) {
@@ -27,7 +28,7 @@ export const generateNotes = async (payload) => {
             payload,
             {
                 withCredentials: true,
-                headers: getAuthHeader() 
+                headers: getAuthHeader()
             }
         );
         return result.data;
@@ -45,7 +46,7 @@ export const downloadPdf = async (result) => {
             {
                 responseType: "blob",
                 withCredentials: true,
-                headers: getAuthHeader() 
+                headers: getAuthHeader()
             }
         )
         const blob = new Blob([response.data], { type: "application/pdf" })
@@ -57,5 +58,31 @@ export const downloadPdf = async (result) => {
         window.URL.revokeObjectURL(url)
     } catch (error) {
         throw new Error("PDF Download Failed")
+    }
+}
+
+export const getMyNotes = async () => {
+    try {
+        const result = await axios.get(serverUrl + "/api/notes/getnotes", {
+            withCredentials: true,
+            headers: getAuthHeader()
+        })
+        return result.data
+    } catch (error) {
+        console.log(error)
+        throw error
+    }
+}
+
+export const getSingleNote = async (noteId) => {
+    try {
+        const result = await axios.get(serverUrl + `/api/notes/${noteId}`, {
+            withCredentials: true,
+            headers: getAuthHeader()
+        })
+        return result.data
+    } catch (error) {
+        console.log(error)
+        throw error
     }
 }
