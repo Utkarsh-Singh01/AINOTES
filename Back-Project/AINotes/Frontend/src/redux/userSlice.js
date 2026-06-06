9b38f59f410e6060622ff927c456524672e97b12
Utkarsh Singh <us6608818@gmail.com>
2026-06-06 10:01:36 +0530

    Update userSlice.js

--- Back-Project/AINotes/Frontend/src/redux/userSlice.js
+++ Back-Project/AINotes/Frontend/src/redux/userSlice.js
@@ -1,20 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice = createSlice({
-  name: "user",
-  initialState: {
-    userData: null,
-  },
-  reducers: {
-    setUserData: (state, action) => {
-      state.userData = action.payload;
+    name: "user",
+    initialState: {
+        userData: null,
+        token: null  
     },
-    updateCredits: (state, action) => {
-      if (state.userData) {
-        state.userData.credits = action.payload;
-      }
+    reducers: {
+        setUserData: (state, action) => {
+            state.userData = action.payload;
+            state.token = action.payload?.token || null
+        },
+        updateCredits: (state, action) => {
+            if (state.userData) {
+                state.userData.credits = action.payload;
+            }
+        },
     },
-  },
 });
 
 export const { setUserData, updateCredits } = userSlice.actions;
